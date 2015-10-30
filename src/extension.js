44d4cdacbd919672aac2b7ab76099bc28d36cc82
Ziya SARIKAYA <sarikayaziya@gmail.com>
2015-10-30 21:38:43 +0200

    code refactoring

--- src/extension.js
+++ src/extension.js
@@ -14,47 +14,42 @@ var exec = require('child_process').exec;
 var parse = require('github-url-from-git');
 
 function findBranch(config) {
-    for (var prop in config) {
-        if (prop.indexOf("branch \"") > -1) {
-            return prop.replace(/branch \"/g, "");
-        }
-    }
+	for (var prop in config) {
+		if (prop.indexOf("branch \"") > -1) {
+			return prop.replace(/branch \"/g, "").replace(/\"/g, "");
+		}
+	}
 
-    return "master";
+	return "master";
 }
 
 function activate() {
-    var cwd = workspace.getPath();
+	var cwd = workspace.getPath();
 
-    commands.registerCommand('openInGihub', function () {
+	commands.registerCommand('openInGitHub', function() {
 
-        git({ cwd: cwd }, function (err, config) {
+		git({
+			cwd: cwd
+		}, function(err, config) {
 
-            var rawUrl = config['remote \"origin\"'].url;
-            var lineIndex = 0;
-            var parsedUri = parse(rawUrl);
-            var branch = findBranch(config);
-            var editor = Window.getActiveTextEditor();
-            var selection = editor.getSelection();
+			var rawUrl = config['remote \"origin\"'].url;
+			var lineIndex = 0;
+			var parsedUri = parse(rawUrl);
+			var branch = findBranch(config);
+			var editor = Window.getActiveTextEditor();
+			var selection = editor.getSelection();
 
-            var currentDocumentUri = editor._document._uri;
-            lineIndex = selection._active._line
+			var currentDocumentUri = JSON.stringify(editor._document._uri);
+			lineIndex = selection._active._line
 
-            var projectName = parsedUri.substring(parsedUri.lastIndexOf("/") + 1, parsedUri.length);
+			var projectName = parsedUri.substring(parsedUri.lastIndexOf("/") + 1, parsedUri.length);
+			var subdir = currentDocumentUri.substring(currentDocumentUri.indexOf(projectName) + projectName.length, currentDocumentUri.length).replace(/\"/g, "");
+			var githubLink = parsedUri + "/blob/" + branch + subdir + "#L" + lineIndex;
 
-            Window.showWarningMessage(JSON.stringify(currentDocumentUri));
-            
-              Window.showWarningMessage(JSON.stringify(currentDocumentUri.indexOf(projectName)));
-              
-            var subdir = currentDocumentUri.substring(currentDocumentUri.indexOf(projectName) + projectName.length + 1, currentDocumentUri.length)
-
-            Window.showWarningMessage(JSON.stringify(subdir));
-
-            var X = parsedUri + "/blob/" + branch + "/" + subdir + "#L" + lineIndex;
-            Window.showWarningMessage(JSON.stringify(X));
-            // exec("start " + uri);
-        });
-    });
+			// Window.showWarningMessage(githubLink);
+			exec("start " + githubLink);
+		});
+	});
 }
 
 exports.activate = activate;
